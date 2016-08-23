c6c21d0d9670eaa9ae32b03de684634bfec8a6a0
Jeevan Sivagnanasuntharam <jeevan@Inmeta.no>
2016-08-23 18:12:40 +0200

    init

---
+++ app/controller.js
@@ -0,0 +1,47 @@
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ * 
+ */
+;(function() {
+
+  angular
+    .module('boilerplate')
+    .controller('MainController', MainController);
+
+  MainController.$inject = ['LocalStorage', 'QueryService', '$scope'];
+
+
+  function MainController(LocalStorage, QueryService, $scope) {
+
+    // 'controller as' syntax
+    var self = this;
+      $scope.test = "dsfsdfsdf";
+      $scope.list = ["dsfsdf", "#"];
+      $scope.data = { textarea: "asdsad" };
+
+      $scope.validate = function ()
+      {
+          var lines = $scope.data.textarea.split('\n');
+          for (var i = 0; i < lines.length; i++) {
+              console.log(lines[i])
+          }
+      }
+
+      ////////////  function definitions
+
+
+      /**
+       * Load some data
+       * @return {Object} Returned object
+       */
+      // QueryService.query('GET', 'posts', {}, {})
+      //   .then(function(ovocie) {
+      //     self.ovocie = ovocie.data;
+      //   });
+  }
+
+
+})();
